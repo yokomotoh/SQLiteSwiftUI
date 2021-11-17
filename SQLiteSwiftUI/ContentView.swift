d49ab337127630cfe21966e67426cfdfd25b3cef
yokomotoh <yokomotohama@hotmail.com>
2021-11-17 11:27:39 +0100

    Initial Commit

---
+++ SQLiteSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SQLiteSwiftUI
+//
+//  Created by vincent schmitt on 17/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SQLiteSwiftUI/SQLiteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteSwiftUIApp.swift
+//  SQLiteSwiftUI
+//
+//  Created by vincent schmitt on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
